1685de5ed36a0193106a029293d0acb5bd16f4ff
Ahmad M Abdelrahman <ahmad18189@gmail.com>
2015-10-04 20:04:01 +0300

    initial commit
    initial commit

---
+++ app/Http/Controllers/usersController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class usersController extends Controller
+{
+    public function profile (){
+        return "lol";
+    }
+}
